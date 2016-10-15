a07c09fb47da47dc0410e0d5fdd91c45f5b54110
Michael <michael.j.feneley@gmail.com>
2016-10-15 08:41:05 -0400

    Don't really like having docstrings below the def... but I think I am going to go back and change to this format for standards sake.

--- stig_apache_2.2_nix/system_auditor.py
+++ stig_apache_2.2_nix/system_auditor.py
@@ -2,11 +2,14 @@
 import subprocess
 import os
 
-"""System auditor checks all system requirements outside of
-apache config documents to make sure the server is in compliance
-with Apache STIG requirements.
-"""
+
 class SystemAuditor:
+
+    """
+    System auditor checks all system requirements outside of
+    apache config documents to make sure the server is in compliance
+    with Apache STIG requirements.
+    """
 
     def __init__(self):
         pass
@@ -15,28 +18,31 @@ class SystemAuditor:
         self.clean_production_server()
         self.check_version()
 
-    """Check SV-32933r1_rule: All web server documentation, sample code,
-	example applications, and tutorials must be removed from a
-	production web server.
-
-    Finding ID: V-13621
-
-    Query the SA to determine if all directories that contain samples and any
-    scripts used to execute the samples have been removed from the server
-
-
-    - If any sample files are found on the web server, this is a finding
-    """
     def clean_production_server(self):
+        """
+        Check SV-32933r1_rule: All web server documentation, sample code,
+        example applications, and tutorials must be removed from a
+        production web server.
+
+        Finding ID: V-13621
+
+        Query the SA to determine if all directories that contain samples and
+        any scripts used to execute the samples have been removed from
+        the server
+
+
+        - If any sample files are found on the web server, this is a finding
+        """
         self.has_documentation()
         self.has_sample_code()
         self.has_tutorials()
 
-    """Check the default installation location for apache documentation
-
-	Currently just checks for the existance of /usr/share/doc/apache2
-    """
     def has_documentation(self):
+        """
+        Check the default installation location for apache documentation
+
+        Currently just checks for the existance of /usr/share/doc/apache2
+        """
         exists = print(os.path.isdir('/usr/share/doc/apache2'))
         exists = os.path.isdir('/usr/share/doc/apache2')
         empty = True
@@ -58,17 +64,17 @@ class SystemAuditor:
     def has_tutorials(self):
         pass
 
-
-    """Check SV-36441r2_rule: Web server software must be a vendor-supported version.
-
-
-    Finding ID: V-2246
-
-    - If the version of Apache is not at the following version or higher, 
-    this is a finding.
-    Apache httpd server version 2.2 - Release 2.2.31 (July 2015)
-    """
     def check_version(self):
+        """Check SV-36441r2_rule: Web server software must be a vendor-supported
+        version.
+
+
+        Finding ID: V-2246
+
+        - If the version of Apache is not at the following version or higher,
+        this is a finding.
+        Apache httpd server version 2.2 - Release 2.2.31 (July 2015)
+        """
         pass
 
 
